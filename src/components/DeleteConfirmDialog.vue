eb657af82e30aa44161e24af8432af349ccdba4f
Maxence Vérité <p.verite.maxence@gmail.com>
2020-09-11 13:18:46 +0200

    Github migration

---
+++ src/components/DeleteConfirmDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+    <v-card-title class="title red darken-2">Delete this {{props.concern}} from the base ?</v-card-title>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn text @click="cancel()">Cancel</v-btn>
+      <v-btn color="error" text @click="confirm()">Confirm</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    concern: String
+  },
+  methods: {
+    confirm() {
+      this.$emit("confirm", true);
+    },
+    cancel() {
+      this.$emit("confirm", false);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.title {
+  color: white;
+}
+</style>
